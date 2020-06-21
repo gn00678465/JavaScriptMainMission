84e220d195b669629c7906c46ea04f81071d0929
MADAO <gn00678465@gmail.com>
2020-06-21 14:23:26 +0800

    layout design

---
+++ week1_todolist/assets/js/index.js
@@ -0,0 +1,14 @@
+;(function (){
+  const data = [
+    {
+      todo: '建立一個to do list',
+      done: false,
+      timetamp: '1234'
+    },
+    {
+      todo: '完成一個to do list',
+      done: false,
+      timetamp: '4567'
+    }
+  ]
+})()
