51da0dfdcff0dd00407f687f966e6b67ea844da3
MADAO <gn00678465@gmail.com>
2020-07-18 14:57:37 +0800

    add form & validation

---
+++ week5_validation/js/index.js
@@ -0,0 +1,32 @@
+// 將 VeeValidate input 驗證工具載入 作為全域註冊
+Vue.component('ValidationProvider', VeeValidate.ValidationProvider);
+// 將 VeeValidate 完整表單 驗證工具載入 作為全域註冊
+Vue.component('ValidationObserver', VeeValidate.ValidationObserver);
+
+// Class 設定檔案
+VeeValidate.configure({
+  classes: {
+    valid: 'is-valid',
+    invalid: 'is-invalid',
+  }
+})
+
+var app = new Vue({
+  el: '#app',
+  data: {
+    payments: ['WebATM','ATM','Barcode','Credit','ApplePay','GooglePay'],
+    payForm: {}
+  },
+  created() {
+    
+  },
+  mounted() {
+    
+  },
+  methods: {
+    
+  },
+  computed: {
+    
+  },
+})
