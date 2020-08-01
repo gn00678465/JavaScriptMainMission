3c7b41d5f583589923d08f1975d571d04edc1f57
MADAO <gn00678465@gmail.com>
2020-08-01 08:14:08 +0800

    update modal

--- week6_VueCli/src/components/Modal.vue
+++ week6_VueCli/src/components/Modal.vue
@@ -1,7 +1,7 @@
 <template>
-  <transition name="modal">
-    <div class="modal" v-if="showModal">
-      <div class="model__container">
+  <transition name="modal" v-if="showModal">
+    <div class="modal" @click.prevent="closeModal">
+      <div class="modal__container">
         <!-- header -->
         <div class="modal__header">
           <slot name="header">
@@ -27,6 +27,11 @@ export default {
       showModal: false,
     };
   },
+  methods: {
+    closeModal() {
+      this.showModal = false;
+    },
+  },
 };
 </script>
 
@@ -38,5 +43,83 @@ export default {
   width: 100%;
   height: 100vh;
   background-color: rgba(0, 0, 0, 0.5);
+  transition: opacity 0.8s ease;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  .modal {
+    &__container {
+      padding: 20px 30px;
+      background-color: #fff;
+      border-radius: 2px;
+      box-shadow: 0 2px 8px rgba(0, 0, 0, 0.33);
+      transition: all 0.8s ease;
+    }
+  }
 }
+
+.modal-enter {
+  opacity: 0;
+}
+
+.modal-leave-active {
+  opacity: 0;
+}
+
+.modal-enter-active .modal__container {
+  animation: bounceInDown .7s forwards;
+}
+.modal-leave-active .modal__container {
+  animation: bounceOutUp .7s forwards;
+}
+// enter
+@keyframes bounceInDown {
+  from,
+  60%,
+  75%,
+  90%,
+  to {
+    animation-timing-function: cubic-bezier(0.215, 0.61, 0.355, 1);
+  }
+
+  0% {
+    opacity: 0;
+    transform: translate3d(0, -3000px, 0) scaleY(3);
+  }
+
+  60% {
+    opacity: 1;
+    transform: translate3d(0, 25px, 0) scaleY(0.9);
+  }
+
+  75% {
+    transform: translate3d(0, -10px, 0) scaleY(0.95);
+  }
+
+  90% {
+    transform: translate3d(0, 5px, 0) scaleY(0.985);
+  }
+
+  to {
+    transform: translate3d(0, 0, 0);
+  }
+}
+// leave
+@keyframes bounceOutUp {
+  20% {
+    transform: translate3d(0, -10px, 0) scaleY(0.985);
+  }
+
+  40%,
+  45% {
+    opacity: 1;
+    transform: translate3d(0, 20px, 0) scaleY(0.9);
+  }
+
+  to {
+    opacity: 0;
+    transform: translate3d(0, -2000px, 0) scaleY(3);
+  }
+}
+
 </style>
